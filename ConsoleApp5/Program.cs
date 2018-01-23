e37ca55ae587134af6018cbde58e91e739f897e1
Raul da Mata Esteves <raul.m.esteves@hotmail.com>
2018-01-23 16:12:15 -0200

    Conjunto de numeros criados

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, X, contIn, contOut;
+            N = int.Parse(Console.ReadLine());
+
+            contIn = 0;
+            contOut = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                X = int.Parse(Console.ReadLine());
+
+                if ((X >= 10) & (X <= 20))
+                {
+                    contIn += 1;
+                }
+                else
+                {
+                    contOut += 1;
+                }
+            }
+            Console.WriteLine(contIn + " in");
+            Console.WriteLine(contOut + " out");
+            Console.ReadLine();
+        }
+    }
+}
